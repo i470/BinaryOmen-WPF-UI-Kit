c80a9302913028e828da610be83e392638bd45cb
ks7788 <ks7788a@outlook.com>
2019-05-13 08:06:52 -0500

    added dashboard wpf prism project

--- BinaryOmen.Wpf.Dashobard.Demo/App.xaml.cs
+++ BinaryOmen.Wpf.Dashobard.Demo/App.xaml.cs
@@ -1,17 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BinaryOmen.Wpf.Dashobard.Demo.Views;
+using Prism.Ioc;
+using Prism.Unity;
 using System.Windows;
 
 namespace BinaryOmen.Wpf.Dashobard.Demo
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
     }
 }
